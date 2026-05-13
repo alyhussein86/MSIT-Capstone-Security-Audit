26813136c63c025e87dd9bdc74216fb7446f4e9a
alyhussein86 <111685594+alyhussein86@users.noreply.github.com>
2026-05-14 01:31:05 +0400

    Add new Test scenario

---
+++ Code/test_correlation_enginge.py
@@ -0,0 +1,12 @@
+import pytest
+from correlate import correlate_vulnerabilities
+
+def test_detect_vuln_03_tls():
+    # White-box test: Validates if the engine identifies TLS 1.0 (0x0301)
+    results = correlate_vulnerabilities('mobsf_scan.json', 'traffic_capture.pcap')
+    assert "VULN-03" in results
+
+def test_secure_traffic_ignore():
+    # Black-box test: Ensures secure TLS 1.3 traffic does not trigger an alert
+    results = correlate_vulnerabilities('mobsf_scan.json', 'traffic_capture1.pcap')
+    assert "VULN-03" not in results
